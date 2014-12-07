636f5bb514e2b382be1c44b4aa28605be50b3c92
Joaquin Nunez <contacto@joaquinnunez.cl>
2014-12-06 18:45:00 -0800

    jQuery Rut
    - Codigo del sitio con documentacion
    - Quitadas las tildes :-(

--- jquery.Rut.js
+++ jquery.Rut.js
@@ -1,4 +1,4 @@
-/* Copyright (c) 2009 José Joaquín Núñez (josejnv@gmail.com) http://joaquinnunez.cl/blog/
+/* Copyright (c) 2009 Jose Joaquin Nunez (josejnv@gmail.com) http://joaquinnunez.cl/blog/
  * Licensed under GPL (http://www.opensource.org/licenses/gpl-2.0.php)
  * Use only for non-commercial usage.
  *
